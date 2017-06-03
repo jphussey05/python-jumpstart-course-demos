6fb05b36dd16eb159d9b22e14f0928890083e753
Jason <jphussey05@gmail.com>
2017-06-02 22:07:49 -0400

    Completed weather app

--- apps/05_weather_client/you_try/README.md
+++ apps/05_weather_client/you_try/README.md
@@ -26,17 +26,35 @@ Quick start: [http://docs.python-requests.org/en/master/user/quickstart/](http:/
 
 **Beautiful soup package**
 
-Quick start: [http://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start](http://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start)
-
+Quick start: [http://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start](http://www.crummy.com/software/BeautifulSoup/bs4/doc/#quick-start)444
 **tuples**
 
     # Create a tuple
-    m = (22.5, 44.234, 19.02, 'strong')    temp = m[0]      # 22.5    quality = m[3]   # 'strong'    print(m) # (22.5, 44.234, 19.02, 'strong')    # tuple unpacking    t, la, lo, q = m
+    m = (22.5, 44.234, 19.02, 'strong')
+
+    temp = m[0]      # 22.5
+    quality = m[3]   # 'strong'
+
+    print(m) # (22.5, 44.234, 19.02, 'strong')
+
+    # tuple unpacking
+    t, la, lo, q = m
 
 **namedtuples**
 
     import collections
     
     # Define a named tuple 'type'
-    Measurement = collections.namedtuple('Measurement',     'temp, lat, long, quality')     # Create an instance of that type of named tuple    m = Measurement(22.5, 44.234, 19.02, 'strong')    temp = m[0]          # old skool    temp = m.temp        # new hotness    quality = m.quality  # new hotness    print(m)    # Measurement(temp=22.5, lat=44.234, 
-        long=19.02, quality='strong')
+    Measurement = collections.namedtuple('Measurement', 
+    'temp, lat, long, quality')
+ 
+    # Create an instance of that type of named tuple
+    m = Measurement(22.5, 44.234, 19.02, 'strong')
+
+    temp = m[0]          # old skool
+    temp = m.temp        # new hotness
+    quality = m.quality  # new hotness
+
+    print(m)
+    # Measurement(temp=22.5, lat=44.234, 
+        long=19.02, quality='strong')
--- apps/05_weather_client/you_try/program.py
+++ apps/05_weather_client/you_try/program.py
@@ -1,5 +1,40 @@
 import requests
+import bs4
+from collections import namedtuple
 
+WeatherReport = namedtuple('WeatherReport',
+                           'cond, temp, scale, loc')
+
+
+def cleanup_text(text: str):
+    if not text:
+        return text
+
+    text = text.strip()
+    return text
+
+
+def find_city_and_state_from_location(city: str):
+    return city.split('\n')[0].strip()
+
+
+def get_weather_from_html(html):
+    soup = bs4.BeautifulSoup(html, 'html.parser')
+    city = soup.find(id='location').find('h1').get_text()
+    condition = soup.find(id='curCond').find(class_='wx-value').get_text()
+    temp = soup.find(id='curTemp').find(class_='wx-value').get_text()
+    scale = soup.find(id='curTemp').find(class_='wx-unit').get_text()
+
+    city = cleanup_text(city)
+    city = find_city_and_state_from_location(city)
+    condition = cleanup_text(condition)
+    temp = cleanup_text(temp)
+    scale = cleanup_text(scale)
+
+    # print(city.strip())
+    # print(condition.strip())
+    # print(temp, scale)
+    return WeatherReport(cond=condition, temp=temp, scale=scale, loc=city)
 
 def main():
     # print the header
@@ -10,22 +45,31 @@ def main():
 
     # get html from web
 
-    get_html(zipcode)
+    html = get_html(zipcode)
+    report = get_weather_from_html(html)
 
-    # parse the html
+    print('The temp in {} is {} and {} {}'.format(
+        report.loc,
+        report.cond,
+        report.temp,
+        report.scale
+    ))
+
     # display the forecast
-    print('hello from main')
 
 
 def print_header():
-    print('-'*30)
+    print('-' * 30)
     print('          WEATHER APP')
-    print('-'*30)
+    print('-' * 30)
     print()
 
 
 def get_html(zipcode):
     url = 'https://www.wunderground.com/weather-forecast/{}'.format(zipcode)
+    response = requests.get(url)
+
+    return response.text
 
 
 if __name__ == "__main__":
